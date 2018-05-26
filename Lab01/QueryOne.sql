90966699787d109598e2e8eb8c3c1286857f4211
Estudiante <mhumanh@vallegrande.edu.pe>
2018-05-26 10:28:20 -0500

    actualizado

--- Lab01/QueryOne.sql
+++ Lab01/QueryOne.sql
@@ -8,3 +8,21 @@ use BDHospital
 
 use AdventureWorksDW2016
 select name from sys.tables;
+
+/*visualizar la informacion detallada de una base de datos*/
+exec sp_helpdb AdventureWorks2016;
+
+
+/* utilizando snipest genera codigo de sintaxis  */
+-- Create a new database called 'colegioMG'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT name
+        FROM sys.databases
+        WHERE name = N'colegioMG'
+)
+CREATE DATABASE colegioMG
+GO
